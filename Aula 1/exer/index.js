c79e49ba9d1c37e966d2a831e2a5f26d5f00493e
Victor Pedro <victor.pdsousa1@gmail.com>
2021-03-15 20:44:48 -0300

    aula 3

---
+++ Aula 1/exer/index.js
@@ -0,0 +1,2 @@
+console.log('Meu nome é "Victor P. de Sousa". Estou aprendendo JavaScript às', 20, 'da noite.');
+console.log("Este", "é", "um", 500);
---
+++ Aula 1/index.js
@@ -0,0 +1,7 @@
+// func que ajuda a debugar o code
+console.log('Victor \"Pedro\"'); // ; é opcional em JS
+console.log("Victor 'Pedro'");
+console.log(`'texto' de "teste"`); // usado p/ template string
+
+console.log(123456, 18.676, 'Nº'); // valor literal, valor real (ponto flutuante) e string
+// tudo é number em JS
