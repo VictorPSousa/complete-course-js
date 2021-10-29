aa71f4fe026e2f6544f5683d07f4bfc488947242
VictorPSousa <victor.pdsousa1@gmail.com>
2021-10-29 19:00:04 -0300

    Aula 5

---
+++ Aula 6/index.js
@@ -0,0 +1,16 @@
+// Regras
+// Nomes de Constantes não podem ser palavras reservadas
+// Constantes precisam ter nomes significativos
+// Nome da variável não pode iniciar com número
+// Não pode possuir espaços ou traços
+// Para separar nomes compostos de Constantes
+// Usar camelCase ou snake_case
+// JS é case-sensitive
+// Não é possível redeclarar Constantes com let
+// Não pode modificar o valor de uma constante
+// Ela deve ser criada e inicializada ao mesmo tempo
+
+const primeiroNumero = 5;
+const segundoNumero = 12;
+const conta = primeiroNumero + segundoNumero;
+console.log(conta)
