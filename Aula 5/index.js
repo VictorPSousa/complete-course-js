164344088cbcc1766f473db8b6ab803eef007acc
VictorPSousa <victor.pdsousa1@gmail.com>
2021-10-29 18:59:57 -0300

    Aula 5

---
+++ Aula 5/index.js
@@ -0,0 +1,16 @@
+let nome = 'João'; // Declara e inicializa a variável - String
+
+console.log(nome, 'nasceu em 1984.');
+console.log('Em 2000', nome, 'conheceu Maria.');
+console.log(nome, 'casou-se com Maria em 2012.');
+console.log('Maria teve 1 filho com', nome, 'em 2015.');
+console.log('O filho de', nome, 'se chama Eduardo.');
+
+// Nomes de variáveis não podem ser palavras reservadas
+// Variáveis precisam ter nomes significativos
+// Nome da variável não pode iniciar com número
+// Não pode possuir espaços ou traços
+// Para separar nomes compostos de variáveis
+// Usar camelCase ou snake_case
+// JS é case-sensitive
+// Não é possível redeclarar variáveis com let
